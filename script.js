8ca8f3e24a3af9c1b3bd70fac210575c14440882
nabanitjee <nabanitself@gmail.com>
2026-06-12 01:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function show(id) {
-    document.getElementById('step' + id).classList.remove('hidden');
+function showContent() {
+    const audio = document.getElementById('bg-music');
+    audio.play().catch(e => console.log("Interaction required for audio"));
+    document.getElementById('intro-view').classList.add('hidden');
+    document.getElementById('content-view').classList.remove('hidden');
 }
